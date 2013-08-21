d04b1776981d2987548bb565094d229004c2d265
Anton <a.gerashchenko@gmail.com>
2013-08-21 19:52:56 +0400

    Первый коммит

---
+++ js/script.js
@@ -0,0 +1,16 @@
+﻿$(function() {
+	setFooter();
+});
+
+
+function setFooter() {
+	var footer = $('footer.footer');
+
+	if (footer.length) {
+		var footerHeight = footer.outerHeight(true);
+		footer.outerHeight(footerHeight);
+		$('.container')
+			.append('<div class="footer-helper" style="height: ' + footerHeight +'px"></div>')
+			.css('marginBottom', -footerHeight);
+	}
+}
